e94f15f39d1d5fba15d59b4b9f229b84d94d6ae5
Hasan Basri Ayhaner <hasan.ayhaner@nextlua.com>
2023-10-06 22:33:20 +0300

    Variable and method names translated to english

--- C_Exercises/Binary_Search_Tree/BST.cpp
+++ C_Exercises/Binary_Search_Tree/BST.cpp
@@ -1,42 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h> // Gerekli kutuphaneleri ekledim.
-#include "tree.h"   // added required and writed libraries
+#include <stdlib.h>
+#include "tree.h"
+// Added required libraries
 
 int main()
 {
-    tree *agac = NULL; // main() fonksiyonunda arama agaclarında kullanmak icin agac veri yapisindan bir pointer olusturdum ve NULL ile isaretledim.
+    // created tree
+    tree *binaryTree = NULL; // main() fonksiyonunda arama agaclarında kullanmak icin binaryTree data yapisindan bir pointer olusturdum ve NULL ile isaretledim.
 
-    int islem = 1;                          // Islem dongusu olusturabilmek icin islem degiskeni olusturdum.
-    int secim, sayi, j, toplam = 0, ort, f; // Secimi, girilen degeri, toplam, ortalama ve sayma sayaci icin degisken atadim.
-    while (islem == 1)                      // Islem degiskeni 1 oldugu surece islemleri sonlandirmayacak while() dongusu olusturdum.
-    {                                       // Kullaniciya islem seceneklerini sundum ve islem numaralarini belirttim.
-        printf("1: Ekle(Add)\n2: Eleman ara(Find node)\n3: Yaprak Ortalamasi(Average of Leaves)\n4: Girilen degerden buyuk verileri bul(Find larger values than the entered value)\n5: Girilen seviyedeki elemanlari bul(Find data of entered level)\n6: Cikis(Exit)\n");
-        scanf("%d", &secim); // Kullanicinin sectigi islemi tutmak icin daha once atadigim degiskeni kullandim.
-        switch (secim)       // Islemleri birbirinden ayirabilmek icin switch-case yapisini kullandim. Girddi olarak kulanicinin secimini aldim.
+    int process = 1, sum = 0, choice, number, j, avg, f; // Secimi, girilen degeri, sum, ortalama ve sayma sayaci icin degisken atadim.
+
+    while (process == 1) // Islem degiskeni 1 oldugu surece islemleri sonlandirmayacak while() dongusu olusturdum.
+    {                    // Kullaniciya process seceneklerini sundum ve process numaralarini belirttim.
+        printf("1: Ekle(Add)\n2: Eleman search(Find node)\n3: Yaprak Ortalamasi(Average of Leaves)\n4: Girilen degerden buyuk verileri bul(Find larger values than the entered value)\n5: Girilen seviyedeki elemanlari bul(Find data of entered level)\n6: Cikis(Exit)\n");
+        scanf("%d", &choice); // Kullanicinin sectigi islemi tutmak icin daha once atadigim degiskeni kullandim.
+        switch (choice)       // Islemleri birbirinden ayirabilmek icin switch-case yapisini kullandim. Girddi olarak kulanicinin secimini aldim.
         {
         case 1: // "1" girilmesi sonucu Kullanicidan eklemek istedigi sayiyi aldim.
             printf("Eklemek istediginiz veriyi girin...\n");
-            scanf("%d", &sayi);
-            agac = ekle(agac, sayi); // ekleme fonksiyonunu agac pointeri ve deger girdisi ile calistirarak ekleme islemi tamamlaninca ekrani temizledim.
+            scanf("%d", &number);
+            binaryTree = add(binaryTree, number); // ekleme fonksiyonunu binaryTree pointeri ve deger girdisi ile calistirarak ekleme islemi tamamlaninca ekrani temizledim.
             system("clear");
             break;
+
         case 2:
-            adimsay = 1; // "2" girilmesi sonucu adimsay degiskenini "1" e esitleyip kullanicidan aramak istedigi elemani girmesini istedim ve girdigi elemani tarayarak aldim.
+            stepCount = 1; // "2" girilmesi sonucu stepCount degiskenini "1" e esitleyip kullanicidan aramak istedigi elemani girmesini istedim ve girdigi elemani tarayarak aldim.
             printf("Aramak istediginiz veriyi girin...\n");
-            scanf("%d", &sayi);
-            ara(agac, sayi); // ara() fonksiyonunu agac pointeri ve kullanicinin girdigi deger ile calistirdim
-            // getch();                                        // fonksiyon sonucu ekrana yazdirinca kullanici herhangi bir tusa basana kadar bilgilerin ekranda kalmasi icin getch() kullandim.
+            scanf("%d", &number);
+            search(binaryTree, number); // search() fonksiyonunu binaryTree pointeri ve kullanicinin girdigi deger ile calistirdim
             getchar();
             fflush(stdin);
             getchar();
-            system("clear"); // Ana islem menusune donmeden once  ekrani temizledim.
+            system("clear"); // Ana process menusune donmeden once  ekrani temizledim.
             break;
-        case 3: // "3" girilmesi sonucu sayaclari ve toplam degiskenini "0"a esitleyerek :
+
+        case 3: // "3" girilmesi sonucu sayaclari ve sum degiskenini "0"a esitleyerek :
             i = 0;
             j = 0;
-            toplam = 0;
-            yaprak(agac); // yaprak() fonksiyonunu agac pointeri ile calistirarak :
-            if (i == 0)   // Agac bos ise kullaniciya bildirip ana menuye yonlendirdim
+            sum = 0;
+            leaf(binaryTree); // leaf() fonksiyonunu binaryTree pointeri ile calistirarak :
+            if (i == 0)       // Agac bos ise kullaniciya bildirip ana menuye yonlendirdim
             {
                 printf("Agac bos!\n");
                 // getch();
@@ -45,60 +48,60 @@ int main()
                 getchar();
                 break;
             }
-            while (j < i) // Agac bos degil ise agactaki yaprak sayisina ulasincaya kadar :
+            while (j < i) // Agac bos degil ise agactaki leaf sayisina ulasincaya kadar :
             {
-                toplam = toplam + yapraklar[j]; // Yapraklari topladim ve eleman sayisini tutan degeri artirdim.
+                sum = sum + leaves[j]; // Yapraklari topladim ve eleman sayisini tutan degeri artirdim.
                 j++;
             }
-            ort = toplam / j; // ortalama degiskenine yaprak degiskenlerinin toplaminin yaprak sayisina bolumunu esitleyerek ekrana yazdirdim.
-            printf("Yaprak dugumlerinin ortalamasi : %d\n", ort);
-            // getch();                                        // Sonucu ekrana yazdirinca kullanici herhangi bir tusa basana kadar bilgilerin ekranda kalmasi icin getch() kullandim.
+            avg = sum / j; // ortalama degiskenine leaf degiskenlerinin toplaminin leaf sayisina bolumunu esitleyerek ekrana yazdirdim.
+            printf("Yaprak dugumlerinin ortalamasi : %d\n", avg);
             getchar();
             fflush(stdin);
             getchar();
             system("clear"); // Ana menuye donmeden once ekrani temizledim.
             break;
+
         case 4: // "4" girilmesi sonucu Kullanicidan , kendisinden buyuk sayilari gormek istedigi degeri girmesini istedim.
             printf("Kendisinden buyuk olan sayilari bulmak istedigin sayiyi gir...\n");
-            scanf("%d", &sayi); // Kullanicinin girdigi sayiyi tarayarak :
-            printf("%d degerinden buyuk elemanlar : ", sayi);
-            kontrol = 0;
+            scanf("%d", &number); // Kullanicinin girdigi sayiyi tarayarak :
+            printf("%d degerinden buyuk elemanlar : ", number);
+            control = 0;
             k = 0;
-            buyukleriBul(agac, sayi); // buyukleriBul() fonksiyonunu agac pointeri ve girilen sayi ile calistirarak.
-            if (kontrol == 0)
-                printf("Yoktur.\n"); // kontrol degiskeni degismemisse girilen degerden buyuk elemanin olmadigini ;
-            else                     // kontrol degiskeni degismisse :
+            findBigElements(binaryTree, number); // findBigElements() fonksiyonunu binaryTree pointeri ve girilen number ile calistirarak.
+            if (control == 0)
+                printf("Yoktur.\n"); // control degiskeni degismemisse girilen degerden buyuk elemanin olmadigini ;
+            else                     // control degiskeni degismisse :
             {
                 for (int i = 0; i < k; i++)
                 {
                     if (i == k - 1) // dizinin son elemaninin sonrasinda "-" isareti koymayacak ;
-                        printf("%d", buyukleriBulDizi[i]);
-                    else // Dizinin ara elemanlari icin eleman arasina "-" koyacak sekilde diziyi yazdirdim.
-                        printf("%d - ", buyukleriBulDizi[i]);
+                        printf("%d", bigElementsArray[i]);
+                    else // Dizinin search elemanlari icin eleman arasina "-" koyacak sekilde diziyi yazdirdim.
+                        printf("%d - ", bigElementsArray[i]);
                 }
             }
             printf("\n");
-            // getch();                                        // Sonucu ekrana yazdirinca kullanici herhangi bir tusa basana kadar bilgilerin ekranda kalmasi icin getch() kullandim.
             getchar();
             fflush(stdin);
             getchar();
             system("clear"); // Ana menuye donmeden once ekrani temizledim.
             break;
+
         case 5: // "5" girilmesi sonucu Kullanicidan , hangi seviyedeki elemanlari gormek istedigi sordum.
             printf("Hangi seviyedeki elemanlari gormek istersiniz...\n");
-            scanf("%d", &sayi); // Gormek istedigi seviyeyi tarayarak degiskene atadim ve :
-            f = 0;              // sayac egiskenlerini "0"a esitleyip :
+            scanf("%d", &number); // Gormek istedigi seviyeyi tarayarak degiskene atadim ve :
+            f = 0;                // sayac egiskenlerini "0"a esitleyip :
             k = 0;
-            printf("%d. Seviyedeki elemanlar : ", sayi);
-            seviye(agac, sayi); // seviye() fonksiyonunu agac pointeri ve kullanicinin girdigi deger ile calistirdim.
-            if (k == 0)         // kullanicinin girdigi seviyede eleman yok ise olmadigini ;
+            printf("%d. Seviyedeki elemanlar : ", number);
+            level(binaryTree, number); // level() fonksiyonunu binaryTree pointeri ve kullanicinin girdigi deger ile calistirdim.
+            if (k == 0)                // kullanicinin girdigi seviyede eleman yok ise olmadigini ;
                 printf(" Yok !");
             for (int i = 0; i < k; i++) // eleman var ise :
             {
                 if (i == k - 1) // sonuncu elemani sonrasina "-" koymadan
-                    printf("%d", seviyeDizi[i]);
-                else // ara elemanlari ise aralarina "-" yerlesitrilecek sekilde ekrana yazdirdim.
-                    printf("%d - ", seviyeDizi[i]);
+                    printf("%d", levelArray[i]);
+                else // search elemanlari ise aralarina "-" yerlesitrilecek sekilde ekrana yazdirdim.
+                    printf("%d - ", levelArray[i]);
             }
             printf("\n");
             // getch();                                        // Sonucu ekrana yazdirinca kullanici herhangi bir tusa basana kadar bilgilerin ekranda kalmasi icin getch() kullandim.
@@ -107,18 +110,16 @@ int main()
             getchar();
             system("clear"); // Ana menuye donmeden once ekrani temizledim.
             break;
+
         case 6: // "6" girilmesi sonucu cikis yapmak icin while dongusunun devamini asglayan degeri "0" a esitleyerek ana menuden cikisi sagladim.
-            islem = 0;
+            process = 0;
             break;
+
         default:
             system("clear"); // Islem menusunde gecersiz giris yapildigi takdirde ekrani temizleyerel kullnaiciyi ana menuye tekrar yonlendirdim.
             break;
         }
     }
-    // getch();
-    // getchar();
-    // fflush(stdin);
-    // getchar();
-    // system("clear"); // Ekrani temizleyerek programi sonlandirdim.
+
     return 0;
 }
--- C_Exercises/Binary_Search_Tree/tree.h
+++ C_Exercises/Binary_Search_Tree/tree.h
@@ -4,116 +4,116 @@
 #include <stdlib.h>
 
 typedef struct Tree // Ikili arama agaci yapisini struct yardimiyla olusturdum.
-{                   // Agacin sayisal veri tutmasi icin bir int degisken atadim.
-    int veri;
-    Tree *sol, *sag; // Agaci iki kola ayirabilmek icin sag ve sol pointerlarini agac veri cinsinden olusturdum.
+{                   // Agacin sayisal data tutmasi icin bir int degisken atadim.
+    int data;
+    Tree *left, *right; // Agaci iki kola ayirabilmek icin right ve left pointerlarini binaryTree data cinsinden olusturdum.
 } tree;
 
-tree *ekle(tree *agac, int x) // Agaca eleman ekleyebilmek icin ekle() fonksiyonunu agac veri cinsinden olusturdum ve girdi olarak agac pointeri ve sayisal veri aldim.
+tree *add(tree *binaryTree, int number) // Agaca eleman ekleyebilmek icin add() fonksiyonunu binaryTree data cinsinden olusturdum ve girdi olarak binaryTree pointeri ve sayisal data aldim.
 {
-    if (agac == NULL) // Agac bos olmasi durumunu if() baglaci ile kontrol ederek bos ise :
+    if (binaryTree == NULL) // Agac bos olmasi durumunu if() baglaci ile control ederek bos ise :
     {
         tree *root = (tree *)malloc(sizeof(tree)); // Agacın kokunu olusturmak icin bellekten yer ayirma amaciyla malloc fonksiyonu kullandim.
-        root->sol = NULL;                          // Agacin sag ve sol dallari bos olacagindan bunlari NULL ile isaretledim.
-        root->sag = NULL;
-        root->veri = x; // Agacin kok degerine kullanicinin girdigi veriyi atadim.
+        root->left = NULL;                          // Agacin right ve left dallari bos olacagindan bunlari NULL ile isaretledim.
+        root->right = NULL;
+        root->data = number; // Agacin kok degerine kullanicinin girdigi veriyi atadim.
         return root;
     }                   // Agac bos degil ise :
-    if (agac->veri < x) // Kullanicinin girmek istedigi veri Agacin kok verisinden buyuk ise agacin sag dalina gitmesi
+    if (binaryTree->data < number) // Kullanicinin girmek istedigi data Agacin kok verisinden buyuk ise agacin right dalina gitmesi
     {
-        agac->sag = ekle(agac->sag, x);
-        return agac;
+        binaryTree->right = add(binaryTree->right, number);
+        return binaryTree;
     }
-    agac->sol = ekle(agac->sol, x); // Kullanicinin girdigi deger Agacin kok verisinden kucuk ise agacin sol dalina gitmesi icin ekle() fonksiyonunu tekrar cagirarak,
-    return agac;                    // Recursive (oz yinelemeli) sekilde veriye uygun yer bulunana kadar ilereleyen bir fonksiyon yapisi olusturdum.
+    binaryTree->left = add(binaryTree->left, number); // Kullanicinin girdigi deger Agacin kok verisinden kucuk ise agacin left dalina gitmesi icin add() fonksiyonunu tekrar cagirarak,
+    return binaryTree;                    // Recursive (oz yinelemeli) sekilde veriye uygun yer bulunana kadar ilereleyen bir fonksiyon yapisi olusturdum.
 }
 
 // Aranan seviyedeki elemanlari listeleyebilmek icin dizi olusturdum.
-int seviyeDizi[100]; /*10*/
+int levelArray[100]; /*10*/
 int f = 0;           // Seviyeyi belirleyebilmek icin iki degisken olusturdum.
 int k = 0;
-void seviye(tree *agac, int sayi) // Aranan seviyedeki elemanlari bulup onlari diziye aktaracak fonksiyonu yazdim.
+void level(tree *binaryTree, int number) // Aranan seviyedeki elemanlari bulup onlari diziye aktaracak fonksiyonu yazdim.
 {
-    if (agac == NULL) // Agac bos ise fonksiyonu durdurdum. Islem yapilirken aranan seviye bos ise bos oldugunu belirtmek icin buradaki degiskenlerden faydalandim.
+    if (binaryTree == NULL) // Agac bos ise fonksiyonu durdurdum. Islem yapilirken wanted level bos ise bos oldugunu belirtmek icin buradaki degiskenlerden faydalandim.
         return;
-    if (f == sayi) // Aranan seviye seviye sayacina esit olunca o seviyedeki elemani diziye aktardim.
+    if (f == number) // Aranan level level sayacina esit olunca o seviyedeki elemani diziye aktardim.
     {
-        seviyeDizi[k] = agac->veri;
+        levelArray[k] = binaryTree->data;
         k++; // Sonraki elemani dizinin sonraki elemanina aktarabilmek icin dizinin eleman sayacini 1 artirdim.(Her bir arama icin dizi eleman sayaci 0'dan baslar.)
     }
-    f++; // Agacin soluna veya sagina gitmesi icin tekrar cagirilan fonksiyon icin seviye sayacini 1 artirdim.
-    seviye(agac->sol, sayi);
-    seviye(agac->sag, sayi);
+    f++; // Agacin soluna veya sagina gitmesi icin tekrar cagirilan fonksiyon icin level sayacini 1 artirdim.
+    level(binaryTree->left, number);
+    level(binaryTree->right, number);
     f--; // Bulunulan dalin solu ve sagi bossa bir onceki dala donerken seviyeyi 1 eksilttim.
 }
 
-int yapraklar[100];     // Yapraklardaki verileri tutup ortalamalarini alabilmek icin bir dizi olusturdum.
+int leaves[100];     // Yapraklardaki verileri tutup ortalamalarini alabilmek icin bir dizi olusturdum.
 int i = 0;              // Diziye eleman ekleyebilmek icin bir degisken atadim.
-void yaprak(tree *agac) // Yapraklari bulan ve yaprakları tutacak diziye aktaran yaprak() fonksiyonunu olusturdum. Fonksiyon girdisi olarak agac veri tipinden pointer aldim.
+void leaf(tree *binaryTree) // Yapraklari bulan ve yaprakları tutacak diziye aktaran leaf() fonksiyonunu olusturdum. Fonksiyon girdisi olarak binaryTree data tipinden pointer aldim.
 {
-    tree *iter;       //  Agacta gezebilmek icin agac veri tipinden bir gezici pointer olusturdum ve recursive yapida bu pointeri kullandim.
-    if (agac == NULL) // Agac bos ise fonksiyonu durdurdum.
+    tree *iter;       //  Agacta gezebilmek icin binaryTree data tipinden bir gezici pointer olusturdum ve recursive yapida bu pointeri kullandim.
+    if (binaryTree == NULL) // Agac bos ise fonksiyonu durdurdum.
         return;
-    if (agac->sol == NULL && agac->sag == NULL) // Agac bos degilse ama bulunulan elemanin sagi ve solu bos ise o eleman yaprak olarak tanimlandigindan o elemani diziye aktardim.
+    if (binaryTree->left == NULL && binaryTree->right == NULL) // Agac bos degilse ama bulunulan elemanin sagi ve solu bos ise o eleman leaf olarak tanimlandigindan o elemani diziye aktardim.
     {
-        yapraklar[i] = agac->veri; // Dizinin eleman sayacini 1 arttirarak sonraki yapragin dizinin sonraki alanina atanmasini sagladim.
+        leaves[i] = binaryTree->data; // Dizinin eleman sayacini 1 arttirarak sonraki yapragin dizinin sonraki alanina atanmasini sagladim.
         i++;
-        iter = agac; // Yaprak bulunduktan sonra gezici pointeri tekrar basa alarak diger yapraklari aramasini sagladim.
+        iter = binaryTree; // Yaprak bulunduktan sonra gezici pointeri tekrar basa alarak diger yapraklari aramasini sagladim.
     }
-    if (agac->sag != NULL) // Bulunulan elemanin sagi bos degil ise :
+    if (binaryTree->right != NULL) // Bulunulan elemanin sagi bos degil ise :
     {
-        iter = agac->sag; // Gezici pointeri agacin sag elemanini gosterecek sekilde atayip fonksiyonu gezici pointer ile tekrar cagirdim.
-        yaprak(iter);
+        iter = binaryTree->right; // Gezici pointeri agacin right elemanini gosterecek sekilde atayip fonksiyonu gezici pointer ile tekrar cagirdim.
+        leaf(iter);
     }
-    if (agac->sol != NULL) // Bulunulan elemanin sagi bos degil ise :
+    if (binaryTree->left != NULL) // Bulunulan elemanin sagi bos degil ise :
     {
-        iter = agac->sol; // Gezici pointeri agacin sol elemanini gosterecek sekilde atayip fonksiyonu gezici pointer ile tekrar cagirdim.
-        yaprak(iter);     // Bu sayede yapraklar diziye aktarilip diziden ortalamalarini almak uzere kullaniliyor.
+        iter = binaryTree->left; // Gezici pointeri agacin left elemanini gosterecek sekilde atayip fonksiyonu gezici pointer ile tekrar cagirdim.
+        leaf(iter);     // Bu sayede leaves diziye aktarilip diziden ortalamalarini almak uzere kullaniliyor.
     }
 }
 
-int adimsay;                    // Aranan elemanin kac adimda bulundugunu tespit edip kullaniciya bildirmek icin adimsay degskeni olusturdum.
-int ara(tree *agac, int aranan) // Kullanicidan alinan veriyi agacta aramak icin ara() fonksiyonunu olusturdum ve girdi olarak agac veri tipinden pointer ve aranan elemani aldim.
+int stepCount;                    // Aranan elemanin kac adimda bulundugunu tespit edip kullaniciya bildirmek icin stepCount degskeni olusturdum.
+int search(tree *binaryTree, int wanted) // Kullanicidan alinan veriyi agacta aramak icin search() fonksiyonunu olusturdum ve girdi olarak binaryTree data tipinden pointer ve wanted elemani aldim.
 {
-    if (agac == NULL) // Aranan eleman bulunamadi ise fonksiyon "-1" degeri dondurerek sonlaniyor ve kullaniciya aranan elemanin bulunamadigini bildiriyor.
+    if (binaryTree == NULL) // Aranan eleman bulunamadi ise fonksiyon "-1" degeri dondurerek sonlaniyor ve kullaniciya wanted elemanin bulunamadigini bildiriyor.
     {
-        printf("%d elemani bulunamadi.\n", aranan);
+        printf("%d elemani bulunamadi.\n", wanted);
         return -1;
     }
-    if (agac->veri == aranan) // Aranan eleman bulundu ise kullaniciya elemanin kac adimda bulundugu bildiriliyor ve fonksiyon "1" degeri dondurerek sonlaniyor.
+    if (binaryTree->data == wanted) // Aranan eleman bulundu ise kullaniciya elemanin kac adimda bulundugu bildiriliyor ve fonksiyon "1" degeri dondurerek sonlaniyor.
     {
-        printf("%d elemani %d adimda bulundu.\n", aranan, adimsay);
+        printf("%d elemani %d adimda bulundu.\n", wanted, stepCount);
         return 1;
     }
 
-    adimsay++;               // Aranan eleman bakilan yerde bulunamadi ise adim sayacini bir artirarak :
-    if (aranan < agac->veri) // Aranan deger bakilan degerden kucuk ise agacin sol tarafina bakilmasi icin fonksşyonu tekrar calistirdim.(recursive)
+    stepCount++;               // Aranan eleman bakilan yerde bulunamadi ise adim sayacini bir artirarak :
+    if (wanted < binaryTree->data) // Aranan deger bakilan degerden kucuk ise agacin left tarafina bakilmasi icin fonksşyonu tekrar calistirdim.(recursive)
     {
-        agac = agac->sol;
-        ara(agac, aranan); /*iter*/
+        binaryTree = binaryTree->left;
+        search(binaryTree, wanted); /*iter*/
     }
-    else // Aranan deger bakilan degerden buyuk ise agacin sag tarafina bakilmasi icin fonksşyonu tekrar calistirdim.(recursive)
+    else // Aranan deger bakilan degerden buyuk ise agacin right tarafina bakilmasi icin fonksşyonu tekrar calistirdim.(recursive)
     {
-        agac = agac->sag;
-        ara(agac, aranan); /*iter*/
-    }                      // Bu sekilde aranan eleman bulunana kadar gerekli yyone gidildi ve eleman bulununca adim sayisi kullaniciya bildirildi ve fonksiyon sonlandirildi.
+        binaryTree = binaryTree->right;
+        search(binaryTree, wanted); /*iter*/
+    }                      // Bu sekilde wanted eleman bulunana kadar gerekli yyone gidildi ve eleman bulununca adim sayisi kullaniciya bildirildi ve fonksiyon sonlandirildi.
     return -1;
 } // Eleman bulunamadi ise kullaniciya elemanin bulunamadigini bildirerek fonksiyonu sonlandirdim.
 
-int buyukleriBulDizi[100];           // Kullanicidan alinan belirli bir degerden buyuk elemanlari tutabilmek ve duzgun listeleybilmek icin bir dizi tanimladim.
-int kontrol = 0;                     // Bulunamamasi durumunda kullaniciya bildirebilmek icin kontrol degiskeni olusturdum.
-void buyukleriBul(tree *agac, int x) // Kullanicinin istedigi bir degerden buyuk elemanlari listeleyebilmesi icin buyukleriBul() fonksiyonu olusturdum.
-{                                    // fonksiyon girdisi olarak agac veri tipinden pointer ve kullanicinin girdigi degeri aldim.
-    if (agac == NULL)                // Eger agac bos ise veya kullanicinin girdigi degerden buyuk deger yok ise fonksiyonu sonlandirdim.
+int bigElementsArray[100];           // Kullanicidan alinan belirli bir degerden buyuk elemanlari tutabilmek ve duzgun listeleybilmek icin bir dizi tanimladim.
+int control = 0;                     // Bulunamamasi durumunda kullaniciya bildirebilmek icin control degiskeni olusturdum.
+void findBigElements(tree *binaryTree, int number) // Kullanicinin istedigi bir degerden buyuk elemanlari listeleyebilmesi icin findBigElements() fonksiyonu olusturdum.
+{                                    // fonksiyon girdisi olarak binaryTree data tipinden pointer ve kullanicinin girdigi degeri aldim.
+    if (binaryTree == NULL)                // Eger binaryTree bos ise veya kullanicinin girdigi degerden buyuk deger yok ise fonksiyonu sonlandirdim.
         return;
-    if (agac->veri > x) // Eger bakilan veri kullanicinin girdigi degerden buyuk ise veriyi daha sonra duzgun sekilde listeleyebilmek icin diziye aktardim.
+    if (binaryTree->data > number) // Eger bakilan data kullanicinin girdigi degerden buyuk ise veriyi daha sonra duzgun sekilde listeleyebilmek icin diziye aktardim.
     {
-        buyukleriBulDizi[k] = agac->veri;
-        kontrol = 1; // Eger kullanicinin girdigi degerden buyuk herhangi bir veri var ise kontrol degiskeninin degerini degistirdim.
+        bigElementsArray[k] = binaryTree->data;
+        control = 1; // Eger kullanicinin girdigi degerden buyuk herhangi bir data var ise control degiskeninin degerini degistirdim.
         k++;         // Daha once tanimladigim dizi eleman sayacini burada tekrar kullandim. Islem yapilrken degeri 0'dan baslatarak diziyi tekrar bastan yazmis oldum.
     }
-    buyukleriBul(agac->sol, x); // Odev bilgilendirmesinde verildigi sekilde preorder olarak arattım.
-    buyukleriBul(agac->sag, x);
+    findBigElements(binaryTree->left, number); // Odev bilgilendirmesinde verildigi sekilde preorder olarak arattım.
+    findBigElements(binaryTree->right, number);
 }
 
 #endif
