362c7fc7b98dd3bc7570f339d64d08f78113c4d3
Hasan Basri Ayhaner <hbasriayhaner114@gmail.com>
2023-10-15 01:00:25 +0300

    Fixed Address(adress) Typo

--- C_Exercises/Pointer/pointer.c
+++ C_Exercises/Pointer/pointer.c
@@ -5,11 +5,11 @@ int main()
 {
     int x = 5;
     int *ptr = &x;
-    printf("x'in degeri(The value of x)................. = %d\n", x);
-    printf("ptr'nin degeri(The value of ptr)............ = %d\n", ptr);
-    printf("*ptr'nin degeri(The value of *ptr).......... = %d\n", *ptr);
-    printf("&x'in adresi(The adress of &x).............. = %d\n", &x);
-    printf("&ptr'nin adresi(The adress of &ptr)......... = %d\n", &ptr);
+    printf("The value of x.......... = %d\n", x);
+    printf("The value of ptr........ = %d\n", ptr);
+    printf("The value of *ptr....... = %d\n", *ptr);
+    printf("The address of &x....... = %d\n", &x);
+    printf("The address of &pt)..... = %d\n", &ptr);
 
     return 0;
 }
