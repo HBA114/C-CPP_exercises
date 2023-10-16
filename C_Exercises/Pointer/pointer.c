1d516164d8afd2c86d1bf7b953df62f8d87f1448
Hasan Basri Ayhaner <hbasriayhaner114@gmail.com>
2023-10-16 14:48:00 +0300

    Updated pointer.c and gitignore Printed pointer with %p

--- C_Exercises/Pointer/pointer.c
+++ C_Exercises/Pointer/pointer.c
@@ -7,10 +7,10 @@ int main()
     int *ptr = &x;
 
     printf("The value of x.......... = %d\n", x);
-    printf("The value of ptr........ = %d\n", ptr);
+    printf("The value of ptr........ = %p\n", ptr);
     printf("The value of *ptr....... = %d\n", *ptr);
-    printf("The address of &x....... = %d\n", &x);
-    printf("The address of &pt)..... = %d\n", &ptr);
+    printf("The address of &x....... = %p\n", &x);
+    printf("The address of &pt)..... = %p\n", &ptr);
 
     return 0;
 }
